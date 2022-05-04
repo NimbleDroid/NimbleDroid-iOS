3341016085525142f0087b72d7d3d8fec675451a
Tom Huzij <tom@headspin.io>
2022-05-04 18:39:05 -0400

    allow optional parameters per the HeadSpinV1 label specification

--- NimbleDroid/NimbleDroid.swift
+++ NimbleDroid/NimbleDroid.swift
@@ -1,33 +1,50 @@
 import Foundation
 
 public class NDScenario : NSObject {
-    @objc public class func begin(bookendID : String) {
-        NSLog("HeadSpinV1 Label.start name:%@", bookendID)
+    @objc public class func begin(bookendID : String, type : String? = nil, category : String? = nil, data : String? = nil) {
+        let nameParam = bookendID
+        var typeParam = ""
+        var categoryParam = ""
+        var dataParam = ""
+        if type != nil {
+            typeParam = " label_type:\(type!)"
+        }
+        if category != nil {
+            categoryParam = " category:\(category!)"
+        }
+        if data != nil {
+            dataParam = " data:\(data!)"
+        }
+        NSLog("HeadSpinV1 Label.start name:%@%@%@%@", nameParam, typeParam, categoryParam, dataParam)
+        //NSLog("HeadSpinV1 Label.start name:%@ label_type:page-load-request category:%@ data:%@", bookendID, category, data)
         fflush(stderr)
     }
 
-    @objc public class func begin(bookendID : String, category : String, data : String) {
-        NSLog("HeadSpinV1 Label.start name:%@ label_type:page-load-request category:%@ data:%@", bookendID, category, data)
+    @objc public class func end(bookendID : String, data : String? = nil) {
+        let nameParam = bookendID
+        var dataParam = ""
+        if data != nil {
+            dataParam = " data:\(data!)"
+        }
+        NSLog("HeadSpinV1 Label.end name:%@%@", nameParam, dataParam)
         fflush(stderr)
     }
 
-    @objc public class func end(bookendID : String) {
-        NSLog("HeadSpinV1 Label.end name:%@", bookendID)
-        fflush(stderr)
-    }
-
-    @objc public class func end(bookendID : String, data : String) {
-        NSLog("HeadSpinV1 Label.end name:%@ data:%@", bookendID, data)
-        fflush(stderr)
-    }
-
-    @objc public class func instant(bookendID : String) {
-        NSLog("HeadSpinV1 Label.instant name:%@", bookendID)
-        fflush(stderr)
-    }
-
-    @objc public class func instant(bookendID : String, category : String, data : String) {
-        NSLog("HeadSpinV1 Label.instant name:%@ label_type:page-load-request category:%@ data:%@", bookendID, category, data)
+    @objc public class func instant(bookendID : String, type : String? = nil, category : String? = nil, data : String? = nil) {
+        let nameParam = bookendID
+        var typeParam = ""
+        var categoryParam = ""
+        var dataParam = ""
+        if type != nil {
+            typeParam = " label_type:\(type!)"
+        }
+        if category != nil {
+            categoryParam = " category:\(category!)"
+        }
+        if data != nil {
+            dataParam = " data:\(data!)"
+        }
+        NSLog("HeadSpinV1 Label.instant name:%@%@%@%@", nameParam, typeParam, categoryParam, dataParam)
         fflush(stderr)
     }
 
